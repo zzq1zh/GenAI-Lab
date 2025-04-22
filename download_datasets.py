3d46510b192fe4ba6ed0e9d55c3b29946fa59490
Ziqi Zhang <zzq1zh@163.com>
2025-04-22 00:44:03 -0400

    optimize training pipeline

--- download_datasets.py
+++ download_datasets.py
@@ -5,8 +5,8 @@ import urllib.request
 target_path = os.path.join("datasets", "preprocess", "eccDNA_Atlas", "Homo_sapiens")
 os.makedirs(target_path, exist_ok=True)
 
-# EccDNA CSV file URL
-csv_url = "https://drive.google.com/file/d/1QrDjwfzg0L8M2eons97N0UPWJTnuB0jr/view?usp=sharing"
+# Save the EccDNA CSV
+csv_url = "https://drive.usercontent.google.com/download?id=1QrDjwfzg0L8M2eons97N0UPWJTnuB0jr&export=download&authuser=0&confirm=t&uuid=029b1ddb-2ffa-490d-96f8-0c815cc13a45&at=APcmpoxnXvkgKt4XilEMzmmjWEd9%3A1744124388589"
 csv_file_path = os.path.join(target_path, "Homo_sapiens.csv")
 
 # Download the EccDNA file 
